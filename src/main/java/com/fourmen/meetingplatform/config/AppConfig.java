809af138123aac2fe16b7ec46df1ce134e062c7f
JG <wprkf1005@gmail.com>
2025-08-21 09:02:15 +0900

    feat: - 추가: WebClient.Builder 빈 설정 메서드 구현 - 개선: WebClient 사용 편의성 향상

--- src/main/java/com/fourmen/meetingplatform/config/AppConfig.java
+++ src/main/java/com/fourmen/meetingplatform/config/AppConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature; // Import 추가
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule; // Import 추가
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +18,9 @@ public class AppConfig {
         // 날짜/시간을 ISO-8601 표준 Timestamp 형식이 아닌, 문자열로 직렬화하도록 설정
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
+    }
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
