8415f204a58d63ee45e64ac7805faed138a0ec5d
JG <wprkf1005@gmail.com>
2025-08-21 09:02:58 +0900

    추가: SearchRequest 및 SearchResponse DTO 클래스 생성
    - 추가: 검색 요청 처리를 위한 SearchRequest DTO 클래스
    - 추가: 검색 응답 처리를 위한 SearchResponse DTO 클래스

---
+++ src/main/java/com/fourmen/meetingplatform/domain/intelligence/dto/SearchResponse.java
@@ -0,0 +1,10 @@
+package com.fourmen.meetingplatform.domain.intelligence.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SearchResponse {
+    private String answer;
+}
