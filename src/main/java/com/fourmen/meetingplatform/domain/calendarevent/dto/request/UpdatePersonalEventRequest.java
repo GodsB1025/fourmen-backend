3602ea6e21e39b0d8e2c445b74c7b020f950b8f8
KKB <godsb1025@gmail.com>
2025-08-21 09:08:35 +0900

    feat: 개인 이벤트 요청 및 업데이트 요청에 시간대 조정 로직 추가

--- src/main/java/com/fourmen/meetingplatform/domain/calendarevent/dto/request/UpdatePersonalEventRequest.java
+++ src/main/java/com/fourmen/meetingplatform/domain/calendarevent/dto/request/UpdatePersonalEventRequest.java
@@ -2,13 +2,31 @@ package com.fourmen.meetingplatform.domain.calendarevent.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UpdatePersonalEventRequest {
     private String title;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+
+    public void setStartTime(LocalDateTime startTime) {
+        if (startTime != null) {
+            this.startTime = startTime.plusHours(9);
+        } else {
+            this.startTime = null;
+        }
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        if (endTime != null) {
+            this.endTime = endTime.plusHours(9);
+        } else {
+            this.endTime = null;
+        }
+    }
 }
