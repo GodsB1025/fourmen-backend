aae61bdb6abbbeb0139e4d77f6e14b34fe6b6c9c
최호철 <145406997+choi-ho-cheol@users.noreply.github.com>
2025-08-26 16:25:57 +0900

    test2

--- src/main/java/com/fourmen/meetingplatform/config/SecurityConfig.java
+++ src/main/java/com/fourmen/meetingplatform/config/SecurityConfig.java
@@ -82,7 +82,10 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "https://kr1-api-object-storage.nhncloudservice.com",
+                "http://localhost:5173"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
